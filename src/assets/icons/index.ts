eaeee0457415bf0cc86bd896eee7172d3c592213
Nick Thiemann <nathiemann1@gmail.com>
2024-07-28 03:27:06 -0500

    Add new icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,48 @@
+import AndroidStudioGradientIcon from "./icons8-android-studio-gradient.svg";
+import AwsGradientIcon from "./icons8-aws-gradient.svg";
+import CssGradientIcon from "./icons8-css-gradient.svg";
+import DjangoGradientIcon from "./icons8-django-gradient.svg";
+import FirebaseIcon from "./icons8-firebase.svg";
+import GithubGradientIcon from "./icons8-github-gradient.svg";
+import GraphQlIcon from "./icons8-graphql.svg";
+import HtmlGradientIcon from "./icons8-html-gradient.svg";
+import JavascriptGradientIcon from "./icons8-javascript-gradient.svg";
+import JiraGradientIcon from "./icons8-jira-gradient.svg";
+import LinkedInGradientIcon from "./icons8-linkedin-gradient.svg";
+import PostgresIcon from "./icons8-postgres.svg";
+import PythonGradientIcon from "./icons8-python-gradient.svg";
+import ReactGradientIcon from "./icons8-react-gradient.svg";
+import SapGradientIcon from "./icons8-sap-gradient.svg";
+import UnityGradientIcon from "./icons8-unity-gradient.svg";
+
+import GitGradientIcon from "./icons8-git-gradient.svg";
+import DockerGradientIcon from "./icons8-docker-gradient.svg";
+import GatsbyIcon from "./icons8-gatsbyjs.svg";
+import MysqlGradientIcon from "./icons8-my-sql-gradient.svg";
+import RaspberryPiGradientIcon from "./icons8-raspberry-pi-gradient.svg";
+import TerraformGradientIcon from "./icons8-terraform.svg";
+
+export {
+  AndroidStudioGradientIcon,
+  AwsGradientIcon,
+  CssGradientIcon,
+  DockerGradientIcon,
+  DjangoGradientIcon,
+  FirebaseIcon,
+  GatsbyIcon,
+  GitGradientIcon,
+  GithubGradientIcon,
+  GraphQlIcon,
+  HtmlGradientIcon,
+  JavascriptGradientIcon,
+  JiraGradientIcon,
+  MysqlGradientIcon,
+  LinkedInGradientIcon,
+  PostgresIcon,
+  PythonGradientIcon,
+  RaspberryPiGradientIcon,
+  ReactGradientIcon,
+  SapGradientIcon,
+  TerraformGradientIcon,
+  UnityGradientIcon,
+};
